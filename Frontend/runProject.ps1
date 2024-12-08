e7e79537e3835debec123ca757d197105a4dc395
RohanBhardwaj-Theprogrammer <149244722+RohanBhardwaj-Theprogrammer@users.noreply.github.com>
2024-12-09 01:29:50 +0530

    Create runProject.ps1

---
+++ Frontend/runProject.ps1
@@ -0,0 +1,8 @@
+# here are the rquired things to be done for the project dependency and libraries used 
+npm install express body-parser ethers dotenv  # installing the packagte through the npm 
+
+
+
+#PRIVATE_KEY=your-private-key-here
+#RPC_URL=https://rpc-mumbai.matic.today
+#CONTRACT_ADDRESS=your-smart-contract-address
